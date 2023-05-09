5c742388e0f0b7ad8d4f4bf1a35065fd168bb3a8
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-09 02:53:20 -0700

    ggg

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(s):
     result = ""
     for c in s:
         result += chr(ord(c) - 32) if 'a' <= c <= 'z' else c
-    print(result, end="\n")
+    print("{}".format(result), end="\n")
 
