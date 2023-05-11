7f92b80028426bbf54fcd40e50878db313df25f1
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-11 05:00:24 -0700

    ggg

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,10 @@ import sys
 if __name__ == "__main__":
     arguments = sys.argv[1:]                                
     num_arguments = len(arguments)
-    print("{:d} arguments:".format(num_arguments))
+    print("{:d} arguments".format(num_arguments), end='')
     if num_arguments > 0:
+        print(":")
         for i, arg in enumerate(arguments):
             print("{}: {}".format(i+1, arg))
+    else:
+        print(".")
