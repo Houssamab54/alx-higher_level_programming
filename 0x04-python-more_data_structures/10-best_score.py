72704d649111fe9505cc39455e9a23d2926098bd
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-17 05:22:37 -0700

    ggg

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python
+def best_score(a_dictionary):
+    l = []
+    for i in a_dictionary:
+        l.append(i)
+    maximum = max(l)
+    for i in a_dictionary.keys():
+        if(a_dictionary[i] == maximum):
+            return i
