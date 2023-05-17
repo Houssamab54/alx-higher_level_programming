eff121f2730a3e6f2fda35b98be240315b98ce9f
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-17 05:43:59 -0700

    ggg

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    l = []
-    for i in a_dictionary.keys():
-        l.append(a_dictionary[i])
-    maximum = max(l)
-    for i in a_dictionary.keys():
-        if(a_dictionary[i] == maximum):
-            return i
+    if a_dictionary != None:
+        l = []
+        for i in a_dictionary.keys():
+            l.append(a_dictionary[i])
+        maximum = max(l)
+        for i in a_dictionary.keys():
+            if(a_dictionary[i] == maximum):
+                return i
+            else:
+                return None
