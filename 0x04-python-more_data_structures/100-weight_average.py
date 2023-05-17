c19faa81a6a3c053c282d665749d6abf5b17ec55
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-17 06:08:07 -0700

    ggg

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    result = (0, 0)
+    for i in my_list[]:
+        result[0] += i[0] * i[1]
+        result[1] += i[1]
+    result[0] = result[0] / result[1]
+    return result
