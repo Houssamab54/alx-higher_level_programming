c6501d2017c15efad862bdd7026940d2aa61ff6c
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-17 03:15:27 -0700

    ggg

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,5 +3,5 @@ def uniq_add(my_list=[]):
     new_list = list(set(my_list)).copy()
     sum = 0
     for i in range(len(new_list)):
-        sum =+ new_list[i]
-    return new_list
+        sum =+ int(new_list[i])
+    return sum
