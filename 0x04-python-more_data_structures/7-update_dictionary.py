ab437e78f4d9a853c56d3cbd9cd9495559bd9d75
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-17 04:55:29 -0700

    ggg

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    a_dictionary[key] = value
+    dictionary = dictionary.copy()
+    dictionary[key] = value
+    return dictionary
