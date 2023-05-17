a6152719d9209279d8b15285152849442e74f8aa
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-17 03:06:45 -0700

    ggg

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list.copy()
-    for new_list[i] in range(len(my_list)):
-        if(i == search):
+    for i in range(len(my_list)):
+        if(new_list[i] == search):
             new_list[i] = replace
     return new_list
