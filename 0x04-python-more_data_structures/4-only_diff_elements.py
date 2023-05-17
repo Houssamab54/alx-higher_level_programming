e539ca23470eefabe82f00df0dfe36a3c59bdf6f
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-05-17 04:22:06 -0700

    ggg

--- 0x04-python-more_data_structures/4-only_diff_elements.py
+++ 0x04-python-more_data_structures/4-only_diff_elements.py
@@ -4,4 +4,7 @@ def only_diff_elements(set_1, set_2):
     for i in set_1:
         if not (i in set_2) and not (i in result):
             result.append(i)
+    for i in set_2:
+        if not (i in set_1) and not (i in result):
+            result.append(i)
     return result
