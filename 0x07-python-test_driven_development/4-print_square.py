20a7271fb7c9523a48b7525e017f47b3cd1d142f
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-06-05 12:51:14 -0700

    ggg

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,11 +1,32 @@
 #!/usr/bin/python3
+
 def print_square(size):
+    """
+    Prints a square made up of "#" characters with each side of the square being `size` characters long.
+
+    Args:
+        size (int): The length of each side of the square.
+
+    Raises:
+        TypeError: If `size` is not an integer.
+        ValueError: If `size` is less than zero.
+
+    Returns:
+        None
+    """
+
+    # Check if `size` is a valid integer value
     if type(size) is not int:
         raise TypeError("size must be an integer")
+
+    # Check if `size` is non-negative
     if size < 0:
         raise ValueError("size must be >= 0")
+
+    # Check if `size` is a valid integer value, even if it is negative
     if size < 0 and type(size) is float:
         raise TypeError("size must be an integer")
 
+    # Print the square
     for i in range(size):
         print("#" * size)
