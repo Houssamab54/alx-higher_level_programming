ebdd690e063c8dea77459ce92844a62a63a95033
Houssamab54 <122807734+Houssamab54@users.noreply.github.com>
2023-06-06 14:33:49 +0200

    Update 19-copy_list.py

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,17 +1,9 @@
 #!/usr/bin/python3
+
+
 def copy_list(l):
+    """generates a copy of a list
     """
-    Returns a copy of a list.
-
-    This function creates a shallow copy of the input list using the slicing operator [:].
-    A shallow copy creates a new list object with the same contents as the original list,
-    but with a different memory address. If the original list contains mutable objects,
-    the new list will still reference the same objects as the original list.
-
-    Parameters:
-    l (list): The list to be copied.
-
-    Returns:
-    list: A shallow copy of the input list.
-    """
+    if not isinstance(l, list):
+        return None
     return l[:]
