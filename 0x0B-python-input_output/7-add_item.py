8da890400cdd8bd19e85e049616e78a2fef16515
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-06-13 04:50:53 -0700

    ggg

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,8 +6,8 @@ from sys import argv
 
 if __name__ == "__main__":
     save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-    fuck_pep8 = __import__('6-load_from_json_file').load_from_json_file
-    load_from_json_file = fuck_pep8
+    load_from_file = __import__('6-load_from_json_file').load_from_json_file
+    load_from_json_file = load_from_file
     filename = "add_item.json"
 
     try:
