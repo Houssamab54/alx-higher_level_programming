e38f809a88ce61ae6f50998db4dc36e01eaa5c38
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-06-16 14:32:56 -0700

    ggg

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -101,10 +101,12 @@ class Rectangle(Base):
             Public method that returns the area of the rectangle.
             """
             return self.__width * self.__height
-    
+
     def display(self):
         """
         Public method that prints the rectangle with the # character.
         """
+        for i in range(self.__y):
+            print()
         for i in range(self.__height):
-            print("#" * self.__width)
+            print(" " * self.__x + "#" * self.__width)
