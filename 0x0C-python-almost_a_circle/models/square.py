076da696a7bef991f9b73bbd9f2f838de7a32da9
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-06-19 09:37:51 -0700

    ggg

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,8 +5,13 @@ from .rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """
+    A class representing a square.
+    """
     def __init__(self, size, x=0, y=0, id=None):
-        """initialise inherited from rectangle"""
+        """
+        initialise inherited from rectangle
+        """
         super().__init__(size, size, x, y, id)
 
     @property
