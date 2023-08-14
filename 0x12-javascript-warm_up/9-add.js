fdf5eee57f5c7d0d9d56bb759d8ab3b25220f405
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-08-14 14:35:44 -0700

    ggg

--- 0x12-javascript-warm_up/9-add.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/*
-  js
-*/
-if (isNaN(parseInt(process.argv[2])) || isNaN(parseInt(process.argv[3]))) {
-  console.log('NaN');
-} else {
-  console.log(parseInt(process.argv[2]) + parseInt(process.argv[3]));
-}
